a5184b4b729fb59426d6861f7ac21bf0785d0371
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2022-08-30 10:34:13 -0400

    Added feature for coarse and fine sums

--- CSVCoarseFinePlugin.py
+++ CSVCoarseFinePlugin.py
@@ -1,12 +1,29 @@
+import PyPluMA
+
 class CSVCoarseFinePlugin:
     def input(self, infile):
-        myfile = open(infile, 'r')
+        #####################################################################
+        # More than one input
+        # Inputfile becomes tab-delimited keyword-value pairs
+        paramfile = open(infile, 'r')
+        self.parameters = dict()
+        for line in paramfile:
+            contents = line.strip().split('\t')
+            self.parameters[contents[0]] = contents[1]
         
+        self.csvfile = PyPluMA.prefix()+"/"+self.parameters["csvfile"]
+        self.doboth = self.parameters["doboth"]
+        #####################################################################
+
+
+    def run(self):
+        myfile = open(self.csvfile, 'r')
         # First line of file
         self.firstline = myfile.readline()
 
         # Dictionary to hold groups
-        self.groups = dict()
+        self.coarsegroups = dict()
+        self.finegroups = dict()
        
         # Read file and sum groups
         for line in myfile:
@@ -15,27 +32,52 @@ class CSVCoarseFinePlugin:
             secondcomma = line.find(',', firstcomma+1)
             coarse = line[:firstcomma]
             fine = line[firstcomma+1:secondcomma]
-            if (coarse not in self.groups):
-               self.groups[coarse] = dict()
-            if (fine not in self.groups[coarse]):
-               self.groups[coarse][fine] = [float(x) for x in line[secondcomma+1:].split(',')]
+            ###############################################################################################
+            # ADDED
+            if (coarse not in self.coarsegroups):
+               self.coarsegroups[coarse] = [float(x) for x in line[secondcomma+1:].split(',')]
             else:
-                self.groups[coarse][fine] += [float(x) for x in line[secondcomma+1:].split(',')]
+                self.coarsegroups[coarse] += [float(x) for x in line[secondcomma+1:].split(',')]
+            ###############################################################################################
+            if (coarse not in self.finegroups):
+               self.finegroups[coarse] = dict()
+            if (fine not in self.finegroups[coarse]):
+               self.finegroups[coarse][fine] = [float(x) for x in line[secondcomma+1:].split(',')]
+            else:
+                self.finegroups[coarse][fine] += [float(x) for x in line[secondcomma+1:].split(',')]
                
-    def run(self):
-         pass
 
     def output(self, outputfile):
-       outfile = open(outputfile, 'w')
+       ########################################################################
+       # We now have a prefix
+       if (self.doboth == "True"):
+          coarsefile = open(outputfile+".coarse.csv", 'w')
+       finefile = open(outputfile+".fine.csv", 'w')
+       ########################################################################
+ 
+       ###################################################################################
+       # Coarse file cannot have fine data in header
+       if (self.doboth == "True"):
+           firstcomma = self.firstline.find(',')
+           secondcomma = self.firstline.find(',', firstcomma+1)
+           coarsefile.write(self.firstline[:firstcomma]+self.firstline[secondcomma:])
+       ###################################################################################
 
        # Write first line
-       outfile.write(self.firstline)
+       finefile.write(self.firstline)
 
-       # Write group sums
-       for coarse in self.groups:
-           for fine in self.groups[coarse]:
-               mylist = str(self.groups[coarse][fine])
+       # Write coarsefine sums
+       for coarse in self.finegroups:
+           ####################################################################
+           # For doing both
+           if (self.doboth == "True"):
+               mylist = str(self.coarsegroups[coarse])
                mylist = mylist[1:len(mylist)-1]
-               outfile.write(coarse+","+fine+","+mylist+"\n")
+               coarsefile.write(coarse+","+mylist+"\n")
+           ####################################################################
+           for fine in self.finegroups[coarse]:
+               mylist = str(self.finegroups[coarse][fine])
+               mylist = mylist[1:len(mylist)-1]
+               finefile.write(coarse+","+fine+","+mylist+"\n")
 
 
